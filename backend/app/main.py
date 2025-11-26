b7995f5af594b0842e34a35e23bfbb0b6d5b50af
javierprias <javier.prias@uniminuto.edu.co>
2025-11-26 09:18:38 -0500

    TEST: Inyectando credenciales hardcodeadas (Debe fallar)

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"mensaje": "Backend de Centinela operando - Estado: Seguro"}
+
+# --- ZONA DE PELIGRO ---
+# Esto es una vulnerabilidad intencional para probar el pipeline
+def connect_to_aws():
+    # Gitleaks detectará este patrón de clave AKIA...
+    aws_access_key_id = "AKIAIMW666S7BELOWKEY" 
+    return "Conectado"
