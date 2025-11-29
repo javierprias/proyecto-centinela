d7776180061851d961fb00cdcf3932d3aefce081
javierprias <javier.prias@uniminuto.edu.co>
2025-11-29 07:02:28 -0500

    Lanzando proyecto a producción

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,9 +35,3 @@ def analizar_noticia(noticia: Noticia):
         "mensaje": "Análisis completado por Centinela AI"
     }
 
-# --- ZONA DE PELIGRO ---
-# Esto es una vulnerabilidad intencional para probar el pipeline
-def connect_to_aws():
-# Gitleaks detectará este patrón de clave AKIA...
-    aws_access_key_id = "AKIAIMW666S7BELOWKEY" 
-    return "Conectado"
